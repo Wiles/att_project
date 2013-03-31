bd917a5948704ea9f061d308f679c8c6dd2b7865
Tom-Bob <kempton.tom@gmail.com>
2013-03-31 11:39:35 -0400

    added base for single web server application

--- GenreNormalizer/GenreNormalizer/Program.cs
+++ GenreNormalizer/GenreNormalizer/Program.cs
@@ -10,43 +10,19 @@ namespace GenreNormalizer
     {
         static void Main(string[] args)
         {
-            var genres = new string[20];
-            genres[1] = "Action";
-            genres[2] = "Adventure";
-            genres[3] = "Animation";
-            genres[4] = "Children";
-            genres[5] = "Comedy";
-            genres[6] = "Crime";
-            genres[7] = "Documentary";
-            genres[8] = "Drama";
-            genres[9] = "Fantasy";
-            genres[10] = "Film-Noir";
-            genres[11] = "Horror";
-            genres[12] = "Musical";
-            genres[13] = "Mystery";
-            genres[14] = "Romance";
-            genres[15] = "Sci-Fi";
-            genres[16] = "Thriller";
-            genres[17] = "War";
-            genres[18] = "Western";
-            genres[19] = "IMAX";
-
-            using (var mStrIn = new FileStream(@"C:\Users\T\Desktop\ml-10m\ml-10M100K\movies.dat", FileMode.Open))
+            using (var mStrIn = new FileStream(@"C:\Users\T\Desktop\data\tags.dat", FileMode.Open))
             using (var reader = new StreamReader(mStrIn))
-            using (var mStrOut = new FileStream(@"movie_genre.dat", FileMode.OpenOrCreate))
-            using (var genreWriter = new StreamWriter(mStrOut))
+            using (var mStrOut = new FileStream(@"tags.csv", FileMode.OpenOrCreate))
+            using (var writer = new StreamWriter(mStrOut))
             {
                 string line;
                 while ((line = reader.ReadLine()) != null)
                 {
                     var c = line.Split(new string[] { "::" }, StringSplitOptions.None);
-                    var g = c[2].Split("|".ToCharArray(), StringSplitOptions.None);
-                    var movieId = c[0];
 
-                    for (int i = 0; i < g.Length; i++)
+                    if (c[1] != "8606")
                     {
-                        var genreId = Array.IndexOf(genres, g[i]);
-                        genreWriter.WriteLine("{0}::{1}", movieId, genreId);
+                        writer.WriteLine("{0}, {1}, '{2}'", c[0], c[1], c[2].Replace("'", "''"));
                     }
                 }
             }
---
+++ web/MovieLens/MovieLens/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MovieLens.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.CurrentTab = "tab_index";
+            return View();
+        }
+
+        public ActionResult Users()
+        {
+            ViewBag.CurrentTab = "tab_users";
+            return View();
+        }
+
+        public ActionResult Movies()
+        {
+            ViewBag.CurrentTab = "tab_movies";
+            return View();
+        }
+
+        public ActionResult Tags()
+        {
+            ViewBag.CurrentTab = "tab_tags";
+            return View();
+        }
+
+        public ActionResult Genres()
+        {
+            ViewBag.CurrentTab = "tab_genres";
+            return View();
+        }
+    }
+}
