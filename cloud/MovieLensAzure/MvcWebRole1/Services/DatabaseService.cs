e3ac243f521d1248c487064ec1b1b4960af5cfbe
Tom-Bob <kempton.tom@gmail.com>
2013-04-06 19:30:39 -0400

    added some shit

---
+++ cloud/MovieLensAzure/MvcWebRole1/Services/DatabaseService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Collections.ObjectModel;
+using MvcWebRole1.Models.DataModels;
+
+namespace MvcWebRole1.Services
+{
+    public class DatabaseService
+    {
+        public Collection<Rating> GetRatings(double userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Collection<Recommendation> GetRecommendations(double userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Collection<Recommendation> GetRecommendationsByMovie(double movieId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- cloud/MovieLensAzure/MvcWebRole1/Services/DistributedService.cs
+++ cloud/MovieLensAzure/MvcWebRole1/Services/DistributedService.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using MvcWebRole1.Models;
+using System.Collections.ObjectModel;
 
 namespace MvcWebRole1.Services
 {
@@ -10,8 +11,20 @@ namespace MvcWebRole1.Services
     {
         public DistributedService(params string[] workers)
         {
+            if (workers.Length == 0)
+            {
+                throw new ArgumentException("list of workers cannot be empty");
+            }
 
+            this.WorkerUrls = new Collection<Uri>();
+
+            foreach (var url in workers)
+            {
+                this.WorkerUrls.Add(new Uri(url));
+            }
         }
+
+        private Collection<Uri> WorkerUrls { get; set; }
 
         public RatingsModel GetRatings(double userId)
         {
